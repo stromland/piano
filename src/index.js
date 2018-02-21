23b7d297df2cc25f412911cdd2d5f446f67d51d8
Espen Strømland <espen.stromland@gmail.com>
2018-02-22 00:15:58 +0100

    midnight bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Scale from './containers/Scale';
+import Scale from './views/Scale';
 import styled from 'styled-components';
 import registerServiceWorker from './registerServiceWorker';
 
