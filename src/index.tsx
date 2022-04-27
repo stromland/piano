18c1a3db48c89a5a0e05b216a1c73bd7961a2ef6
Espen Strømland <espen.stromland@gmail.com>
2022-04-27 18:42:29 +0200

    chore(deps): react 18 ++

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import React from "react";
 
 import { Main } from "views/Main/Main";
 
 import "./index.css";
 
-ReactDOM.render(<Main />, document.getElementById("root"));
+const elementId = "root";
+const container =  document.getElementById(elementId);
+if (container === null) {
+  throw new Error(`could not find element with id=${elementId}`);
+}
+
+const root = createRoot(container);
+root.render(<Main />);
